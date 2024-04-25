1c770f42a6e754fc6f8cff52a41742a30b0ba65e
Oliver Sim <choo-howe.sim@edag.com.my>
2024-04-26 01:25:06 +0800

    init prisma

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,15 @@
-// // ecosystem.config.js
-// module.exports = {
-//   apps: [
-//     {
-//       name: 'nestjs-app',
-//       script: './dist/main.js',
-//       cwd: '/dist/main.js',
-//       watch: true,
-//       env: {
-//         NODE_ENV: 'production',
-//       },
-//     },
-//   ],
-// };
+// ecosystem.config.js
+// this is for configuring if using PM2 instead
+module.exports = {
+  apps: [
+    {
+      name: 'nestjs-app',
+      script: './dist/main.js',
+      cwd: '/dist/main.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
